3adefb0455ff3a1bf0eb55266a7aca0d246fd9ec
Maxime Forget-Séguin <maxime.forget-seguin@radio-canada.ca>
2016-05-26 16:49:21 -0400

    koco2...

--- src/validated-form-control-ui.js
+++ src/validated-form-control-ui.js
@@ -1,9 +1,6 @@
 //    Copyright (c) CBC/Radio-Canada. All rights reserved.
 //    Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-import template from 'text!./validated-form-control.html';
-
-
 var ValidatedFormControl = function(params, componentInfo) {
     var self = this;
 
